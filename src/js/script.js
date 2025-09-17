164ac69221c8e1f8b58cb357814a92201118422b
bbaddoo <bbaddoo@luc.edu>
2025-09-16 23:54:07 -0500

    deploy retry

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 function send_message() {
 
-    document.getElementById("messageContainer").innerHTML = " I hope you are doing well! (:";
+    document.getElementById("messageContainer").innerHTML = " I hope you are doing well!! (:";
 
 }
